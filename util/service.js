83e98b904ecefddaac568e9e568c0ee60206dcef
lunhui1994 <aqshaofei@vip.qq.com>
2020-06-03 16:35:29 +0800

    fix getSign playurl

--- util/service.js
+++ util/service.js
@@ -2,6 +2,7 @@
 
 const axios = require('axios')
 const util = require('./index.js')
+const getSign = require('../vander/sign.js')
 
 const regExp = util.regExp;
 const json2url = util.json2url;
@@ -11,6 +12,13 @@ const json2url = util.json2url;
  */
 let musicInterface = axios.create({
     baseURL: 'https://c.y.qq.com',
+    timeout: 2 * 1000
+})
+/**
+ * @TODO 音乐url axios实例
+ */
+let musicTokenInterface = axios.create({
+    baseURL: 'https://u.y.qq.com',
     timeout: 2 * 1000
 })
 /**
@@ -26,7 +34,7 @@ let welfareInterface = axios.create({
 const apiUrl = {
     searchUrl: '/soso/fcgi-bin/client_search_cp',
     albumimgUrl: 'http://imgcache.qq.com/music/photo/album_300/',
-    tokenUrl: '/base/fcgi-bin/fcg_music_express_mobile3.fcg',
+    tokenUrl: '/cgi-bin/musics.fcg',
     lyricUrl: '/lyric/fcgi-bin/fcg_query_lyric_new.fcg',
     top100Url: '/v8/fcg-bin/fcg_v8_toplist_cp.fcg?g_tk=5381&uin=0&format=json&inCharset=utf-8&outCharset=utf-8¬ice=0&platform=h5&needNewCode=1&tpl=3&page=detail&type=top&topid=27&_=1519963122923',
     welfareUrl: '/api/data/福利/',
@@ -72,28 +80,54 @@ function getMusicList(params, callback) {
     * @param: filename 文件名
     */
 function getMusicToken(params, lyricData) {
+    let data = {
+        "req": {
+            "module": "CDN.SrfCdnDispatchServer",
+            "method": "GetCdnDispatch",
+            "param": {
+                "guid": params.guid,
+                "calltype": 0,
+                "userip": ""
+            }
+        },
+        "req_0": {
+            "module": "vkey.GetVkeyServer",
+            "method": "CgiGetVkey",
+            "param": {
+                "guid": params.guid,
+                "songmid": [
+                    params.songmid
+                ],
+                "songtype": [
+                    0
+                ],
+                "uin": "562032497",
+                "loginflag": 1,
+                "platform": "20"
+            }
+        },
+        "comm": {
+            "uin": 562032497,
+            "format": "json",
+            "ct": 24,
+            "cv": 0
+        }
+    }
+    let signStr = getSign(data);
     let params_data = {
-        format: 'json205361747',
-        platform: 'yqq',
-        cid: '205361747',
-        guid: params.guid, //126548448 5043253136
-        songmid: params.songmid,
-        filename: 'C400' + params.songmid + '.m4a',
-        aggr: 1,
-        flag_qc: 0,
-        cr: 0
+        sign: signStr,
+        data: data
     }
     return new Promise(resolve => {
-        musicInterface({
+        musicTokenInterface({
             method: 'get',
             url: apiUrl.tokenUrl,
             params: params_data
         }).then(res => {
-            let musicUrl = "http://ws.stream.qqmusic.qq.com/" + res.data.data.items[0].filename + "?fromtag=0&guid=" + params.guid + '&vkey=' + res.data.data.items[0].vkey
-            
+            let musicUrl = "https://ws.stream.qqmusic.qq.com/" + res.data.req_0.data.midurlinfo[0].purl;
             let lyric = (lyricData && lyricData.code == '0') ? lyricData.lyric : '无';
             resolve({
-                vkey: res.data.data.items[0].vkey,
+                vkey: res.data.req.data.vkey,
                 musicUrl: musicUrl,
                 lyric: lyric
             });
